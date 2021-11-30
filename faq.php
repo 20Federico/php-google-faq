f54c30134f91789027d7909f4aa5a6aba8d76eb7
20Federico <fefe.molino@gmail.com>
2021-11-30 23:29:13 +0100

    exercise completed

--- faq.php
+++ faq.php
@@ -39,6 +39,59 @@
         "Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati modificati nel rispetto delle leggi europee per la protezione dei dati. Mostriamo questa notifica agli utenti europei quando cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione."
       ]
     ],
+    [
+      "title" => "Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?",
+      "paragraphs" => [
+        "Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.",
+        "Siamo costantemente al lavoro per garantire un'elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.",
+        "Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel <a href='#'>Centro Google per la sicurezza online.</a>",
+        "<a href='#'>Scopri</a> quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.",
+        ]
+    ],
+    [
+      "title" => "Perché il mio account è associato a un paese?",
+      "paragraphs" => [
+        "Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:",
+        " <ol class='ordered_list'>
+            <li> 
+              La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
+              <ol type='a'>
+                <li>
+                  Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.
+                </li>
+                <li>
+                  Google LLC, con sede negli Stati Uniti, per il resto del mondo.
+                </li>
+              </ol>
+            </li>
+            <li>
+              La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.
+            </li>
+          </ol>",
+        "Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.",
+      ]
+    ],
+    [
+      "title" => "",
+      "paragraphs" => [
+        "<h4>Stabilire il paese associato al tuo account</h4>",
+        "Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell'ultimo anno.",
+        "I viaggi frequenti solitamente non influiscono sul paese associato al tuo account. Se ti trasferisci in un altro paese, potrebbe occorrere circa un anno per aggiornare l'associazione del paese.",
+        "Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe essere la differenza tra il paese in cui lavori e il paese in cui risiedi, l'installazione di una rete privata virtuale (VPN) per mascherare il tuo indirizzo IP oppure la residenza vicino a un confine territoriale. <a href='#'>Contattaci</a> se ritieni che il paese associato al tuo account sia sbagliato.",
+      ]
+    ],
+    [
+      "title" => "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?",
+      "paragraphs" => [
+        "I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi <a href='#'>contattare il webmaster</a> del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, <a href='#'>fai clic qui.</a> Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile <a href='#'>visitare la nostra pagina di assistenza per avere ulteriori informazioni.</a>",
+      ]
+    ],
+    [
+      "title" => "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?",
+      "paragraphs" => [
+        "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di <a href='#'>URL referrer.</a> Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili <a href='#'>qui.</a> Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio.",
+      ]
+    ]
   ];
 
 ?>
@@ -49,16 +102,17 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link rel="stylesheet" href="css/style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
   <title>Domande frequenti - Privacy e termini</title>
 </head>
 <body>
   <header>
-    <nav class="px-3">
-      <div class="py-2 d-flex align-items-center">
+    <nav class="px-4">
+      <div class="pt-3 pb-2 d-flex align-items-center">
         <img src="img/Google-Logo.png" alt="Google logo">
-        <h2 class="d-inline-block fs-5 m-0 ps-1 ">Privacy e termini</h2>
+        <h2 class="d-inline-block m-0 ps-1 ">Privacy e termini</h2>
       </div>
       <ul class="m-0 p-0">
         <?php
@@ -89,14 +143,16 @@
         foreach ($sections as $section) { 
 
         echo "<div class='section'>";
-        echo    "<h3>"; 
-        echo      $section["title"]; 
-        echo    "</h3>";
+        if (strlen($section["title"]) > 0) {
+          echo    "<h3>"; 
+          echo      $section["title"]; 
+          echo    "</h3>";
+        };
 
           foreach ($section["paragraphs"] as $paragraph) {
-            echo "<p>";
+            echo "<p> ";
             echo   $paragraph;
-            echo "</p>";
+            echo " </p>";
           };
 
         echo "</div>";
@@ -106,7 +162,23 @@
     </div>
   </main>
   <footer>
+    <div class="container d-flex justify-content-between align-items-center py-2">
+      <ul>
+        <li><a href="#">Google</a></li>
+        <li><a href="#">Tutto su Google</a></li>
+        <li><a href="#">Privacy</a></li>
+        <li><a href="#">Termini</a></li>
+      </ul>
+      <form action="">
+        <label for="select_lang"><i class="fas fa-globe"></i></label>
+        <select name="select_lang" id="select_lang">
+          <option value="italian">Italiano</option>
+          <option value="english">Inglese</option>
+          <option value="spanish">Spagnolo</option>
+        </select>
+      </form>
 
+    </div>
   </footer>
 </body>
 </html>
